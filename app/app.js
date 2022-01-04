13f48f112c18df53a3a9f9eba81e56cfa4d120e4
stefannytoror <stefannyrami@gmail.com>
2022-01-04 09:35:41 -0500

    Add routes and files

---
+++ app/app.js
@@ -0,0 +1,16 @@
+var express = require ('express'),
+var app = express();
+
+app.get('/', function (req, res){
+  res.send({message: 'Home'});
+});
+
+app.get('/apple-app-site-association', function (req, res){
+  res.download('apple-app-site-association');
+});
+
+app.get('/upgrade/', function (req, res){
+  res.send({message: 'Upgrade'});
+});
+
+module.exports = app;
